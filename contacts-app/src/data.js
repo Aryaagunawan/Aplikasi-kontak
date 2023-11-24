c7946420726bff0c3e62f698b71b8e66d6135846
Arya gunawan <147965614+Aryaagunawan@users.noreply.github.com>
2023-11-25 00:39:23 +0700

    Add files via upload

---
+++ contacts-app/src/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+    return [
+        {
+            id: 1,
+            name: 'Arya gunawan',
+            tag: 'aryakk558',
+            imageUrl: '/images/arya.jpg',
+        },
+        {
+            id: 2,
+            name: 'ammarindia',
+            tag: 'marindia',
+            imageUrl: '/images/default.jpg',
+        },
+        {
+            id: 3,
+            name: 'rifkisekincau',
+            tag: 'kincau',
+            imageUrl: '/images/default.jpg',
+        },
+    ];
+}
+
+export { getData };
